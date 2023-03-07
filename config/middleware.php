1ec511a769b72a791ee3392f1f4863ae165b422c
Winv <pro.dorianlopez@gmail.com>
2023-03-07 20:16:38 +0100

    feat: method not allowed

--- config/middleware.php
+++ config/middleware.php
@@ -15,4 +15,12 @@ return function (App $app) {
     $errorHandler = $errorMiddleware->getDefaultErrorHandler();
     $errorHandler->registerErrorRenderer('application/json', ErrorRenderer::class);
     $errorHandler->forceContentType('application/json');
+
+    $errorMiddleware->setErrorHandler(HttpMethodNotAllowedException::class, function (ServerRequestInterface $request, Throwable $exception, bool $displayErrorDetails): Response
+    {
+        $response = new Response();
+        $response->getBody()->write('405 method not allowed.');
+    
+        return $response->withStatus(405);
+    });
 };
