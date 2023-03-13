e888ab0d26ba0c97af35dd2b5b7f1df9600d4eb1
winvdaking <pro.dorianlopez@gmail.com>
2023-03-13 10:45:19 +0100

    fix extends exception

--- lbs_order_service/src/errors/exceptions/ModelExceptionMethod.php
+++ lbs_order_service/src/errors/exceptions/ModelExceptionMethod.php
@@ -2,9 +2,7 @@
 
 namespace orders\errors\exceptions;
 
-use Slim\Exception\HttpSpecializedException;
-
-class OrderExceptionMethod extends HttpSpecializedException
+class OrderExceptionMethod extends \Exception
 {
     protected $code = 405;
     protected $message = 'La méthode contenue dans la rêquete reçue n\'est pas autorisée sur l\'uri indiquée ; cette uri est cependant valide.';
